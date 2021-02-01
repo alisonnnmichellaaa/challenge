c1b1808eb3af44c8222344e6426302ebbd3033df
alisonnnmichell <alisonnnmichell@gmail.com>
2021-02-01 10:13:08 -0300

    Instead of returning just objects in the controller now it returns ResponseEntity. The endpoint /user/{id} was added

--- src/main/java/challenge/Controllers/UserController.java
+++ src/main/java/challenge/Controllers/UserController.java
@@ -1,11 +1,15 @@
 package challenge.Controllers;
 
-import challenge.Dtos.UserDto;
+import challenge.Model.Users.User;
 import challenge.Services.JSONPlaceholderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,17 +19,24 @@ public class UserController {
     @Autowired
     JSONPlaceholderService jsonPlaceholderService;
 
-    public JSONPlaceholderService getJsonPlaceholderService() {
-        return jsonPlaceholderService;
-    }
-
-    public void setJsonPlaceholderService(JSONPlaceholderService jsonPlaceholderService) {
-        this.jsonPlaceholderService = jsonPlaceholderService;
-    }
-
     @GetMapping("/users")
-    public List<UserDto> getUsers() {
-        return Arrays.asList(this.getJsonPlaceholderService().allUsers());
+    public ResponseEntity<List<User>> getUsers() {
+        try {
+            return new ResponseEntity<>(Arrays.asList(this.jsonPlaceholderService.allUsers()), HttpStatus.OK);
+        } catch (Exception exception) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUsers(@PathVariable String id) {
+            try {
+                return new ResponseEntity<>(this.jsonPlaceholderService.findUserById(Long.valueOf(id)), HttpStatus.OK);
+            } catch (Exception exception) {
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+    }
+
+
 
 }
