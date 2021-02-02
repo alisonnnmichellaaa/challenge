d1fb0d6faf135c9613b584c4677801c5be03ef15
alisonnnmichella <alisonnnmichella@gmail.com>
2021-02-02 14:21:05 -0300

    PhotoController test

--- src/main/java/challenge/Controllers/PhotosController.java
+++ src/main/java/challenge/Controllers/PhotosController.java
@@ -9,9 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
-import java.util.List;
-
 @RestController
 public class PhotosController {
 
@@ -21,20 +18,13 @@ public class PhotosController {
 
     @GetMapping("/photos")
     public ResponseEntity<Photo[]> getUsers() {
-        try {
             return new ResponseEntity<>( this.jsonPlaceholderService.allPhotos(),HttpStatus.OK);
-        } catch (Exception exception) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
     }
 
     @GetMapping("/photos/{id}")
     public ResponseEntity<Photo> getUsers(@PathVariable String id) {
-        try {
-            return new ResponseEntity<>( this.jsonPlaceholderService.findPhotoById(Long.valueOf(id)),HttpStatus.OK);
-        } catch (Exception exception) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return new ResponseEntity<>( this.jsonPlaceholderService.findPhotoByUserId(Long.valueOf(id)),HttpStatus.OK);
+
     }
 
 }
